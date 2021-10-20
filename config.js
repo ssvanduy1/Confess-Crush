5e22607084706e183baedbb1922e3ccbc54dad5e
ssvanduy1 <92850944+ssvanduy1@users.noreply.github.com>
2021-10-20 16:56:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ const CONFIG = {
     question: 'Hehehe em cảm ơn nhiều nha kaka !! <3',
     btnReply: 'Có gì gửi đến Duy không nè! <3',
     reply: 'Duy dễ thương quá nè !!!!!!!!!!!!!!!!',
-    mess: 'Hihi, Chị cũng dễ thương ghê !!',
+    mess: 'Hihi, Chị cũng dễ thương quá nè !!',
     messDesc: 'Năm sau em lại chúc nữa nhé!!',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://123anhdep.net/wp-content/uploads/2016/01/chiem-nguong-nhung-bo-hoa-hong-dep-nhat-the-gioi-danh-tang-nguoi-yeu-1.jpg' //link mess của các bạn. VD: https://m.me/nam.nodemy
